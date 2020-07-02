5f3a9a6bd6d3fc8013539ae4c2113068dc66d7c4
Caskey, Damon V <dc@caskeys.com>
2020-07-02 13:07:45 -0400

    Import Terry folder with model text and resources.

---
+++ data/chars/terry/scripts/ani_p.c
@@ -0,0 +1,12 @@
+/*
+Animation script: 
+
+Terry Bogard (PC)
+*/
+
+#include "data/scripts/dc_disney/main.c"
+#include "data/scripts/dc_elmers/main.c"
+#include "data/scripts/dc_fidelity/main.c"
+#include "data/scripts/dc_gauntlet/main.c"
+#include "data/scripts/dc_velocity/main.c"
+
