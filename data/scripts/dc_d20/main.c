cde71989f34d625fc644b1c0c5a833c82be4f6c1
Caskey, Damon V <dc@caskeys.com>
2020-07-02 18:04:43 -0400

    Import dc_d20 random roller.

---
+++ data/scripts/dc_d20/main.c
@@ -0,0 +1,8 @@
+// Package Files
+#include   "data/scripts/dc_d20/config.h"
+
+#import    "data/scripts/dc_d20/random.c"
+#import    "data/scripts/dc_d20/radius.c"
+#import    "data/scripts/dc_d20/negative.c"
+
+
