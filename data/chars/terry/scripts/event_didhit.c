be4ec71b8be5a5fedc1d4c0bafe7730fdc0b4c90
Caskey, Damon V <dc@caskeys.com>
2020-08-11 08:24:22 -0400

    Only draw in opponents hit by Rising Tackle, and only if not blocked.

--- data/chars/terry/scripts/event_didhit.c
+++ data/chars/terry/scripts/event_didhit.c
@@ -3,19 +3,27 @@
 void main()
 {
 	void taker = getlocalvar("damagetaker");
+	void ent = getlocalvar("self");
+	int blocked = getlocalvar("blocked");
+
+	int animation_id = get_entity_property(ent, "animation_id");
+	
+	// If this is rising tackle and they didn't block 
+	// it, use the dc elmers adjustment function to 
+	// draw taker in.
+	if (animation_id == openborconstant("ANI_FREESPECIAL6") && !blocked)
+	{
+		dc_elmers_set_entity(taker);
+
+		dc_elmers_set_offset_x(20);
+		dc_elmers_set_offset_y(0);
+
+		dc_elmers_set_position_adjust_quantity_x(5);
+		dc_elmers_set_position_adjust_quantity_y(5);
+
+		dc_elmers_adjust_distance_to_x();
+		dc_elmers_adjust_distance_to_y();
+	}
 
 	
-
-	//dc_elmers_quick_spot(taker);
-
-	dc_elmers_set_entity(taker);
-
-	dc_elmers_set_offset_x(20);
-	dc_elmers_set_offset_y(0);
-
-	dc_elmers_set_position_adjust_quantity_x(5);
-	dc_elmers_set_position_adjust_quantity_y(5);
-
-	dc_elmers_adjust_distance_to_x();
-	dc_elmers_adjust_distance_to_y();
 }
